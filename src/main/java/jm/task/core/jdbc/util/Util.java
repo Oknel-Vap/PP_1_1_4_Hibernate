01b92f85b25155fa21be4d66b44d09afe59a324e
Oknel-Vap <5lennok5@mail.ru>
2022-11-17 21:02:22 +0300

    .

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,9 +5,9 @@ import java.sql.SQLException;
 import java.sql.DriverManager;
 
 public class Util {
-    static final String db_url = "jdbc:postgresql://localhost:5432/pp_base";
-    static final String user = "postgres";
-    static final String pass = "1234";
+    private static final String db_url = "jdbc:postgresql://localhost:5432/pp_base";
+    private static final String user = "postgres";
+    private static final String pass = "1234";
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("org.postgresql.Driver");
         Connection connection = DriverManager.getConnection(db_url,user,pass);
